b35d4365362a3c3b77dfc1a71ad8ff36260f06ff
Trevor <tallain88@gmail.com>
2019-05-10 18:12:15 -0400

    added in prototype for introduction page

---
+++ resources/js/components/IntroductionComponent.vue
@@ -0,0 +1,21 @@
+<!-- This component will house the template for the introduction 
+     segment of the survey
+-->
+
+<template>
+    <div id="app">
+        
+        <b-navbar class="header" type="dark" variant="info" fixed="top">
+
+            <b-navbar-brand href="#">Check In</b-navbar-brand>
+
+        </b-navbar>
+ 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
