bf1b1b758ed506f103779148ee00e6e85fd403f0
Trevor <tallain88@gmail.com>
2019-05-10 18:12:47 -0400

    added in sub component prototype for 'plan' section

---
+++ resources/js/components/PlanComponent.vue
@@ -0,0 +1,17 @@
+<!-- This component will house the 'plan' segment
+     of the introduction
+-->
+
+<template>
+    <div>
+        
+ 
+ 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
