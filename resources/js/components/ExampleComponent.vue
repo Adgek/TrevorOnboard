ad3465e49c7b15c16ab331d37dd4273eeb82a82e
Trevor <tallain88@gmail.com>
2019-05-10 18:11:43 -0400

    added in new example vue for testing

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+                <div class="card card-default">
+                    <div class="card-header">Main Vue</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        This is the main vue
                     </div>
                 </div>
             </div>
@@ -15,9 +15,56 @@
 </template>
 
 <script>
+
+
     export default {
+
+       data: ()=>({
+            
+            surveyJson: null,
+            jsonObject: {},
+            survey: {
+                pages: null,
+            }
+            
+        }),
+
+        methods: {
+                    getSurveyObject() {
+                        
+                        axios.get('/SendCheckInInfo').then((response) => {
+                            this.surveyJson = response.data.survey;
+                            //this.jsonObject = JSON.parse(response.data);
+                            //this.survey = response.data;    
+                            console.log(response.data);
+                            console.log(this.surveyJson);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                    },
+
+                    testFunction() {
+                        axios.get('/SanityCheck').then(function(response) {
+                            console.log(response.data);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+
+                        console.log("Test function worked bro!");
+                    }
+        },
+
         mounted() {
-            console.log('Component mounted.')
-        }
+            console.log('Component mounted.');
+            this.getSurveyObject();
+            this.testFunction();
+        },
+        
+        
+           
+       
+       
     }
 </script>
