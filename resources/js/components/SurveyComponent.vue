7a67ce48f30694eabfd372c7ef1e005a856ef8d0
Trevor <tallain88@gmail.com>
2019-05-13 19:03:29 -0400

    cleaned up a little and added in other vues (plan and last day)

--- resources/js/components/SurveyComponent.vue
+++ resources/js/components/SurveyComponent.vue
@@ -7,7 +7,7 @@
         <div id="app"  >
 
             <!-- Navbar at the top of screen TODO: centre it! -->       
-            <div class="text-center" id="top">
+            <div justify-center id="top">
 
                 <b-navbar class="header bottom-pad" type="dark" variant="info" fixed="top">
 
@@ -30,28 +30,85 @@
 
                             <!-- total pages with headers - used to step back and forth -->
                             <!-- TODO: template based on total pages -->
-                            <v-stepper-header> Check In
+                            <v-stepper-header  v-if="survey">
 
                                 <v-stepper-step step="1" :complete="step > 1">
-                                 
+                                    
+                                    {{survey.pages[0].name}} <!-- TODO: figure out a way to get page names better -->
 
                                 </v-stepper-step>
 
                                 <v-divider></v-divider>
 
-                            </v-stepper-header>
-                            
-                            <!-- Introduction -->
-                            <v-stepper-content step="1">
+                                <v-stepper-step step="2" :complete="step > 2">
 
+                                    {{survey.pages[1].name}}
+
+                                </v-stepper-step>
+
+                                <v-divider></v-divider>
+
+                                <v-stepper-step step="3" :complete="step > 3">
+
+                                    {{survey.pages[2].name}}
+
+                                </v-stepper-step>
+
+                                <v-divider></v-divider> 
                                 
+                                <v-stepper-step step="4" :complete="step > 4">
 
-                            </v-stepper-content>
+                                    {{survey.pages[3].name}}
+
+                                </v-stepper-step>
+
+                                <v-divider></v-divider>
+
+                                <v-stepper-step step="5" :complete="step > 5">
+
+                                    {{survey.pages[4].name}}
+
+                                </v-stepper-step>
+
+                                <v-divider></v-divider>
+                            </v-stepper-header>
+                            <v-stepper-items  v-if="survey">
+                                <v-stepper-content step="1">
+
+                                    <introduction-component :pageInfo=survey.pages[0]></introduction-component>
+                                </v-stepper-content>
+                                <v-stepper-content step="2">
+                                    <myBody-component  :myBody=survey.pages[1] vbind:title="My Body"></myBody-component>
+
+                                </v-stepper-content>
+                            </v-stepper-items>
+                            <!-- Introduction -->
+                            
+
+                             <v-container grid-list-md text-xs-center>
+                                <v-layout row wrap>
+                                
+                                    <v-flex>
+                                        <v-btn v-scroll-to="'#top'" flat v-if="step > 0" @click.native="step-- ">Previous</v-btn>
+                                        
+                                    </v-flex>
+                                    <v-flex>
+                                        <!-- Basic progress bar. Just a placeholder, for now -->
+                                        <v-progress-linear v-model="progressBarPercentage"></v-progress-linear>
+
+                                    </v-flex>
+                                    <v-flex>
+                                        <v-btn  color="primary" @click.native="step++">Next</v-btn>
+
+                                    </v-flex>
+                                    
+                                </v-layout>
+                            </v-container> 
 
                         </v-stepper>
-
-                        <!-- Basic progress bar. Just a placeholder, for now -->
-                        <v-progress-linear v-model="progressBarPercentage"></v-progress-linear>
+                        
+                               
+                        
 
                     </v-container>
 
@@ -67,17 +124,59 @@
 
 <script>
 
+    //import the introduction component
+   import IntroductionComponent from './IntroductionComponent.vue'
+
     //This vue will simply hold the main common features of the 
     //survey i.e. => progres bar, next/prev buttons and the main header.
 
     export default {
 
         data: ()=>({
-                
+                surveyJson: null,
+                surveyAnswers: null,
+   
+                survey: {
+                    pages: null,
+                },
                 progressBarPercentage: 10,
                 step: 1
                 
-            })
+            }),
+
+            methods: {
+                    getSurveyObject() {
+                        
+                        axios.get('/SendCheckInInfo').then((response) => {
+                            this.surveyJson = response.data.survey;
+                            this.survey.pages = this.surveyJson.pages;
+                            //this.jsonObject = JSON.parse(response.data);
+                            //this.survey = response.data;    
+                            console.log(response.data);
+                            console.log(this.surveyJson);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                    },
+
+                    testFunction() {
+                        axios.get('/SanityCheck').then(function(response) {
+                            console.log(response.data);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+
+                        console.log("Test function worked bro!");
+                    }
+        },
+
+        mounted() {
+            console.log('Component mounted.');
+            this.getSurveyObject();
+            //this.testFunction();
+        },
     }
 
 </script>
