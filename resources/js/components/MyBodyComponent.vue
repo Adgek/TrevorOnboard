16f7e8ff4173e18bcb99a40c39fa5c558dc06010
Trevor <tallain88@gmail.com>
2019-05-14 18:06:15 -0400

    added more in depth skeleton for mybody page

--- resources/js/components/MyBodyComponent.vue
+++ resources/js/components/MyBodyComponent.vue
@@ -25,25 +25,61 @@
                         
                         <v-card-text v-if="questions[0]" >
                             <v-card-actions class="justify-center" >
-                                <div>
+                                <div >
                                     <v-flex v-for="n in 5" :key=n >
                                         <!-- TODO: dynamically get total number of scale choices -->
-                                            <v-btn block>{{questions[0].scale[6 - n]}}</v-btn>
+                                            <v-btn id="energyLevelQuestion" block  v-on:click="handleButton($event)">{{questions[0].scale[6 - n]}}</v-btn>
                                             
                                         
+                                    </v-flex>
+                                    <v-flex v-if="this.energyQuestionTrigger == true">
+                                        <v-text-field box :label="questions[1].question" v-model="surveyAnswers.pages.myBody.answers['Why is energy low?']"></v-text-field>
                                     </v-flex>
                                 </div>
                             </v-card-actions>
                             
                         </v-card-text>
-                        <v-divider></v-divider>
-                        <v-card-title>
-                            <div>
-                            </div>
-                        </v-card-title>
-                        <v-card-text>
+                    </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+        <v-card>
+            <v-container fluid grid-list-lg>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                    <v-card color="white" class="black--text">                
+                        
+                        <v-card-title primary-title class="justify-center">
                         <div>
+                            <div class="headline">
+                                <v-layout row  v-if="questions[0]">
+                                    <v-flex >
+                                        {{questions[2].question}}
+                                    </v-flex>
+                                    
+                                    
+                                </v-layout>
+                            </div>
+                            
                         </div>
+                        
+                        </v-card-title>
+                        <v-card-text v-if="questions[2]" >
+                            <v-card-actions class="justify-center" >
+                                <div >
+                                    <v-flex v-for="n in 2" :key=n >
+                                        <!-- TODO: dynamically get total number of scale choices -->
+                                            <v-btn id="eatDrinkQuestion" block  v-on:click="handleButton($event)">{{questions[2].scale[2 - n]}}</v-btn>
+                                            
+                                        
+                                    </v-flex>
+                                    <v-flex v-if="this.eatDrinkQuestionTrigger == true">
+                                        <v-text-field box :label="questions[3].question" v-model="surveyAnswers.pages.myBody.answers['Why didnt I eat right?']"></v-text-field>
+                                    </v-flex>
+                                </div>
+                            </v-card-actions>
+                            
                         </v-card-text>
                         
                         
@@ -59,7 +95,9 @@
 export default {
 
     data: ()=> ({
-        questions: {} //question object for my body page
+        questions: {}, //question object for my body page
+        energyQuestionTrigger: false,
+        eatDrinkQuestionTrigger: false
     }),
 
     mounted() {
@@ -71,6 +109,7 @@ export default {
 
     props: {
         myBody : {},
+        surveyAnswers: {},
         title: {
             type: String,
             default: 'My Body'
@@ -80,8 +119,49 @@ export default {
     },
 
     methods: {
-        
+        handleButton: function(buttonSource){
+
+            var targetID = buttonSource.currentTarget.id;
+            var targetContent = buttonSource.currentTarget.textContent;
+            
+            
+            if (targetID == "energyLevelQuestion")
+            {
+                if (targetContent == "Bad" || targetContent == "Terrible")
+                {
+                    this.energyQuestionTrigger = true;
+                    console.log(targetContent);
+                    
+                }
+                else
+                {
+                    //reset values since text box is no longer valid
+                    this.energyQuestionTrigger = false;
+                    surveyAnswers.pages.myBody.answers['Why is energy low?'] = null;
+                }
+
+                this.surveyAnswers.pages.myBody.answers['How is my energy level?'] = targetContent;
+                
+            }
+            else if(targetID == "eatDrinkQuestion")
+            {
+                if (targetContent == "No")
+                {
+                    this.eatDrinkQuestionTrigger = true;
+                }
+                else
+                {
+                    //reset values since text box is no longer valid
+                    this.eatDrinkQuestionTrigger = false;
+                    this.surveyAnswers.pages.myBody.answers['Why didnt I eat right?'] = null;
+
+                }
+
+                this.surveyAnswers.pages.myBody.answers['Did I eat and drink right?'] = targetContent;
+            }
         }
+        
+    }
 }
  
 
