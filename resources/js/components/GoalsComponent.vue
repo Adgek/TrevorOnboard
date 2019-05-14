1e12c26f591eb7d2a4804a0548762655dccc266d
Trevor <tallain88@gmail.com>
2019-05-14 18:05:04 -0400

    added new goals component (still figuring out how to put object into combobox

---
+++ resources/js/components/GoalsComponent.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <h3>{{title}}</h3>
+
+        <v-card>
+            <v-container fluid grid-list-lg>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                    <v-card color="white" class="black--text">
+                        <v-card-title primary-title class="justify-center">
+                        <div>
+                            <div class="headline">
+                                <v-layout row  v-if="goals[1]">
+                                    <v-flex >
+                                        What is your skiing goal today?
+                                    </v-flex>
+                                    
+                                    
+                                </v-layout>
+                            </div>
+                            
+                        </div>
+                        
+                        </v-card-title>
+                        
+                        <v-card-text v-if="goals[1]" >
+                            <v-card-actions class="justify-center" >
+                                <div >
+                                    <v-flex >
+                                        <!-- TODO: dynamically get total number of scale choices -->
+                                            <v-select :items="goals" box label="Select a goal" ></v-select>
+                                            
+                                        
+                                    </v-flex>
+                                    
+                                </div>
+                            </v-card-actions>
+                            
+                        </v-card-text>
+                    </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+
+    data: ()=> ({
+        goals: [], //goal object (needs to be an array for the drop down menu)
+        equipmentQuestionTrigger: false
+    }),
+
+    mounted() {
+        this.goals = this.myGoals.goals;
+        console.log(this.goals);
+
+
+    },
+
+    props: {
+        myGoals : {},
+        surveyAnswers: {},
+        title: {
+            type: String,
+            default: 'My Skiing'
+        }
+
+        
+    },
+
+    methods: {
+        handleButton: function(buttonSource){
+
+            var targetID = buttonSource.currentTarget.id;
+            var targetContent = buttonSource.currentTarget.textContent;
+            
+            
+            
+        }
+        
+    }    
+}
+</script>
